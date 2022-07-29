07d6df1bde786299fafc0607f0c07052b65e17fa
Avi1200 <99733279+Avi1200@users.noreply.github.com>
2022-07-29 13:15:51 +0530

    Converting json file to dictionary format and printing the data into excel sheet

---
+++ JsonDatatable/JavaScriptSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonDatatable
+{
+    internal class JavaScriptSerializer
+    {
+        internal T Deserialize<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JsonDatatable/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Collections;
+using GemBox.Spreadsheet;
+using Aspose.Cells.Utility;
+using DocumentFormat.OpenXml.Spreadsheet;
+using ThirdParty.Json.LitJson;
+using OfficeOpenXml;
+using System.Linq;
+using Ganss.Excel;
+
+namespace JsonDatatable
+{
+    class program
+    {
+
+
+         static void Main(string[] args)
+        {
+               string strResultJson = String.Empty;
+                strResultJson = System.IO.File.ReadAllText(@"Data.json");
+
+                var objResponse1 =
+                JsonConvert.DeserializeObject<List<Student>>(strResultJson);
+                var a = objResponse1[0];
+                var b = objResponse1[1];
+                Console.WriteLine("-------------------------");
+                Console.WriteLine(a.ToString());
+                Console.WriteLine(b.ToString());
+                ExcelMapper mapper = new ExcelMapper();
+                var newFile = @"C:\Users\Avishikta\source\repos\JsonDatatable\dataExcel.xlsx";
+                mapper.Save(newFile,objResponse1,"SheetName",true);
+                Console.ReadKey();
+        }
+    }
+}
+        
+    
+
+
+
+
+
+            
+
+
+    
+    
+       
+
+     
+
+
+
+    
