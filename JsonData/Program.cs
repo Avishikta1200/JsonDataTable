faca8a646e97809c802f6d8244ac7c24c5c20ee3
Avi1200 <99733279+Avi1200@users.noreply.github.com>
2022-07-29 15:18:50 +0530

    Converting json file to dictionary format and printing it to excel sheet

---
+++ JsonData/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Collections;
+using Ganss.Excel;
+
+namespace JsonDatatable
+{
+    class program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string strResultJson = String.Empty;
+            strResultJson = System.IO.File.ReadAllText(@"Data.json");
+
+            var objResponse1 =
+            JsonConvert.DeserializeObject<List<Student>>(strResultJson);
+            var a = objResponse1[0];
+            var b = objResponse1[1];
+            Console.WriteLine("-------------------------");
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(b.ToString());
+            ExcelMapper mapper = new ExcelMapper();
+            var newFile = @"C:\Users\Avishikta\source\repos\JsonData\dataExcel.xlsx";
+            mapper.Save(newFile, objResponse1, "SheetName", true);
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ JsonData/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDatatable
+{
+
+    class Student
+    {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+
+
+        public override string ToString()
+        {
+            return string.Format("Customer Information:\nCustomerID: {0}\nCompanyName: {1}\n,ContactName: {2}\nContactTitle: {3}\nAddress: {4}\nCity: {5}\nRegion: {6}\nPostalCode: {7}\nCountry: {8}\nPhone: {9}\nFax: {10}\n", CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax);
+        }
+
+    }
+}
+
